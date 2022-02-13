a1df53cb0b539c37b823d1207974d7fd2e8b39b1
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-13 23:33:07 +0530

    almost done with mobile

---
+++ script.js
@@ -0,0 +1,76 @@
+
+function showfirst()
+{
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+    let third = document.getElementById('third');
+    let fourth = document.getElementById('fourth');
+    let fifth = document.getElementById('fifth');
+    first.style.display = 'flex';
+    second.style.display = 'none';
+    third.style.display = 'none';
+    fourth.style.display = 'none';
+    fifth.style.display = 'none';    
+    // first.style.position = 'absolute'; 
+
+}
+function showsecond()
+{
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+    let third = document.getElementById('third');
+    let fourth = document.getElementById('fourth');
+    let fifth = document.getElementById('fifth');
+    first.style.display = 'none';
+    second.style.display = 'flex';
+    third.style.display = 'none';
+    fourth.style.display = 'none';
+    fifth.style.display = 'none'    
+    // first.style.position = 'absolute'; 
+
+}
+function showthird()
+{
+    let first = document.getElementById('first');
+    let third = document.getElementById('third');
+    let second = document.getElementById('second');
+    let fourth = document.getElementById('fourth');
+    let fifth = document.getElementById('fifth');
+    first.style.display = 'none';    
+    second.style.display = 'none';
+    third.style.display = 'flex';
+    fourth.style.display = 'none';
+    fifth.style.display = 'none';
+    // first.style.position = 'absolute'; 
+
+}
+function showfourth()
+{
+    let fourth = document.getElementById('fourth');
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+    let third = document.getElementById('third');
+    let fifth = document.getElementById('fifth');
+    first.style.display = 'none';    
+    second.style.display = 'none';
+    third.style.display = 'none';
+    fourth.style.display = 'flex';
+    fifth.style.display = 'none';
+    // first.style.position = 'absolute'; 
+
+}
+function showfifth()
+{
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+    let third = document.getElementById('third');
+    let fourth = document.getElementById('fourth');
+    let fifth = document.getElementById('fifth');
+    fifth.style.display = 'flex';
+    second.style.display = 'none';
+    third.style.display = 'none';
+    fourth.style.display = 'none';
+    first.style.display = 'none';    
+    // first.style.position = 'absolute'; 
+
+}
