9e3fe35088ee2c8675fd82ddc6d8acebf00fc3e2
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-15 23:04:57 +0530

    done with the javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-
+  
+  
 
 function showfirst()
 {
@@ -15,6 +15,30 @@ function showfirst()
     let fourthq = document.getElementById('fourthq');
     let fifthq = document.getElementById('fifthq');
 
+    let firstdown = document.getElementById('firstarrdown');
+  let seconddown = document.getElementById('secondarrdown');
+  let thirddown = document.getElementById('thirdarrdown');
+  let fourthdown = document.getElementById('fourtharrdown');
+  let fifthdown = document.getElementById('fiftharrdown');
+  let secondarr = document.getElementById('secondarr');
+     let firstarr  = document.getElementById('firstarr');
+     let thirdarr = document.getElementById('thirdarr');
+     let fourtharr = document.getElementById('fourtharr');
+     let fiftharr = document.getElementById('fiftharr');
+    
+     firstarr.style.display = 'none';
+     firstdown.style.display = 'initial';
+     secondarr.style.display = 'initial';
+     seconddown.style.display= 'none';
+     thirddown.style.display = 'none'
+     thirdarr.style.display = 'initial';
+     fourtharr.style.display = 'initial';
+     fourthdown.style.display = 'none';
+     fiftharr.style.display = 'initial';
+     fifthdown.style.display = 'none';
+
+     
+ 
    
     first.style.display = 'flex';
     second.style.display = 'none';
@@ -22,11 +46,11 @@ function showfirst()
     fourth.style.display = 'none';
     fifth.style.display = 'none';   
     
-    firstq.style.fontWeight= 'bold';
+    firstq.style.fontWeight= 'bold'; firstq.style.color = 'black';
     secondq.style.fontWeight = 'initial';
     thirdq.style.fontWeight = 'initial';
     fourthq.style.fontWeight = 'initial';
-    fifth.style.fontWeight = 'initial';
+    fifthq.style.fontWeight = 'initial';
 }
 function showsecond()
 {
@@ -36,11 +60,34 @@ function showsecond()
     let fourth = document.getElementById('fourth');
     let fifth = document.getElementById('fifth');
 
+    let firstdown = document.getElementById('firstarrdown');
+  let seconddown = document.getElementById('secondarrdown');
+  let thirddown = document.getElementById('thirdarrdown');
+  let fourthdown = document.getElementById('fourtharrdown');
+  let fifthdown = document.getElementById('fiftharrdown');
+
     let firstq = document.getElementById('firstq');
     let secondq = document.getElementById('secondq');
     let thirdq = document.getElementById('thirdq');
     let fourthq = document.getElementById('fourthq');
     let fifthq = document.getElementById('fifthq');
+    let secondarr = document.getElementById('secondarr');
+    let firstarr  = document.getElementById('firstarr');
+    let thirdarr = document.getElementById('thirdarr');
+    let fourtharr = document.getElementById('fourtharr');
+    let fiftharr = document.getElementById('fiftharr');
+
+     secondarr.style.display = 'none';
+     seconddown.style.display= 'initial';
+     firstarr.style.display = 'initial';
+     firstdown.style.display = 'none';
+     thirddown.style.display = 'none'
+     thirdarr.style.display = 'initial';
+     fourtharr.style.display = 'initial';
+     fourthdown.style.display = 'none';
+     fiftharr.style.display = 'initial';
+     fifthdown.style.display = 'none';
+
 
     first.style.display = 'none';
     second.style.display = 'flex';
@@ -49,10 +96,10 @@ function showsecond()
     fifth.style.display = 'none';
 
     firstq.style.fontWeight= 'initial';
-    secondq.style.fontWeight = 'bold';
+    secondq.style.fontWeight = 'bold';secondq.style.color = 'black';
     thirdq.style.fontWeight = 'initial';
     fourthq.style.fontWeight = 'initial';
-    fifth.style.fontWeight = 'initial';
+    fifthq.style.fontWeight = 'initial';
    
 }
 function showthird()
@@ -63,12 +110,34 @@ function showthird()
     let fourth = document.getElementById('fourth');
     let fifth = document.getElementById('fifth');
 
+    let firstdown = document.getElementById('firstarrdown');
+  let seconddown = document.getElementById('secondarrdown');
+  let thirddown = document.getElementById('thirdarrdown');
+  let fourthdown = document.getElementById('fourtharrdown');
+  let fifthdown = document.getElementById('fiftharrdown');
+  let secondarr = document.getElementById('secondarr');
+    let firstarr  = document.getElementById('firstarr');
+    let thirdarr = document.getElementById('thirdarr');
+    let fourtharr = document.getElementById('fourtharr');
+    let fiftharr = document.getElementById('fiftharr');
+
+    thirddown.style.display = 'initial'
+    thirdarr.style.display = 'none';
+     secondarr.style.display = 'initial';
+     seconddown.style.display= 'none';
+     firstarr.style.display = 'initial';
+     firstdown.style.display = 'none';
+     fourtharr.style.display = 'initial';
+     fourthdown.style.display = 'none';
+     fiftharr.style.display = 'initial';
+     fifthdown.style.display = 'none';
+
     let firstq = document.getElementById('firstq');
     let secondq = document.getElementById('secondq');
     let thirdq = document.getElementById('thirdq');
     let fourthq = document.getElementById('fourthq');
     let fifthq = document.getElementById('fifthq');
-
+    
     first.style.display = 'none';    
     second.style.display = 'none';
     third.style.display = 'flex';
@@ -77,9 +146,9 @@ function showthird()
    
    firstq.style.fontWeight= 'initial';
     secondq.style.fontWeight = 'initial';
-    thirdq.style.fontWeight = 'bold';
+    thirdq.style.fontWeight = 'bold'; thirdq.style.color = 'black';
     fourthq.style.fontWeight = 'initial';
-    fifth.style.fontWeight = 'initial';
+    fifthq.style.fontWeight = 'initial';
 }
 function showfourth()
 {
@@ -89,12 +158,35 @@ function showfourth()
     let third = document.getElementById('third');
     let fifth = document.getElementById('fifth');
 
+    let firstdown = document.getElementById('firstarrdown');
+  let seconddown = document.getElementById('secondarrdown');
+  let thirddown = document.getElementById('thirdarrdown');
+  let fourthdown = document.getElementById('fourtharrdown');
+  let fifthdown = document.getElementById('fiftharrdown');
+  let secondarr = document.getElementById('secondarr');
+    let firstarr  = document.getElementById('firstarr');
+    let thirdarr = document.getElementById('thirdarr');
+    let fourtharr = document.getElementById('fourtharr');
+    let fiftharr = document.getElementById('fiftharr');
+
+  secondarr.style.display = 'initial';
+     seconddown.style.display= 'none';
+     firstarr.style.display = 'initial';
+     firstdown.style.display = 'none';
+     thirddown.style.display = 'none'
+     thirdarr.style.display = 'initial';
+     fourtharr.style.display = 'none';
+     fourthdown.style.display = 'initial';
+     fiftharr.style.display = 'initial';
+     fifthdown.style.display = 'none';
+
     let firstq = document.getElementById('firstq');
     let secondq = document.getElementById('secondq');
     let thirdq = document.getElementById('thirdq');
     let fourthq = document.getElementById('fourthq');
     let fifthq = document.getElementById('fifthq');
 
+    
     first.style.display = 'none';    
     second.style.display = 'none';
     third.style.display = 'none';
@@ -104,8 +196,10 @@ function showfourth()
     firstq.style.fontWeight= 'initial';
     secondq.style.fontWeight = 'initial';
     thirdq.style.fontWeight = 'initial';
-    fourthq.style.fontWeight = 'bold';
-    fifth.style.fontWeight = 'initial';
+    fourthq.style.fontWeight = 'bold'; fourthq.style.color = 'black';
+    fifthq.style.fontWeight = 'initial'; 
+
+
 }
 function showfifth()
 {
@@ -114,13 +208,36 @@ function showfifth()
     let third = document.getElementById('third');
     let fourth = document.getElementById('fourth');
     let fifth = document.getElementById('fifth');
+
+    let firstdown = document.getElementById('firstarrdown');
+  let seconddown = document.getElementById('secondarrdown');
+  let thirddown = document.getElementById('thirdarrdown');
+  let fourthdown = document.getElementById('fourtharrdown');
+  let fifthdown = document.getElementById('fiftharrdown');
      
     let firstq = document.getElementById('firstq');
     let secondq = document.getElementById('secondq');
     let thirdq = document.getElementById('thirdq');
     let fourthq = document.getElementById('fourthq');
     let fifthq = document.getElementById('fifthq');
+    let secondarr = document.getElementById('secondarr');
+     let firstarr  = document.getElementById('firstarr');
+     let thirdarr = document.getElementById('thirdarr');
+     let fourtharr = document.getElementById('fourtharr');
+     let fiftharr = document.getElementById('fiftharr');
 
+    secondarr.style.display = 'initial';
+     seconddown.style.display= 'none';
+     firstarr.style.display = 'initial';
+     firstdown.style.display = 'none';
+     thirddown.style.display = 'none'
+     thirdarr.style.display = 'initial';
+     fourtharr.style.display = 'initial';
+     fourthdown.style.display = 'none';
+     fiftharr.style.display = 'none';
+     fifthdown.style.display = 'initial';
+
+    
     fifth.style.display = 'flex';
     second.style.display = 'none';
     third.style.display = 'none';
@@ -131,5 +248,5 @@ function showfifth()
     secondq.style.fontWeight = 'initial';
     thirdq.style.fontWeight = 'initial';
     fourthq.style.fontWeight = 'initial';
-    fifthq.style.fontWeight = 'bold';
+    fifthq.style.fontWeight = 'bold'; fifthq.style.color = 'black';
 }
